3d283a2196018b641b15660a985266183564e7a4
GameDev <gamemaster@GME.local>
2023-08-15 15:14:06 -0500

    Configuration for SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #Create an instance of the Flask class
 app = Flask(__name__)
-# COnfigure our app with a secret key
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+# COnfigure our app with a secret key from the config class
+app.config.from_object(Config)
+
+# Create an instance of SQLAlchemy to represent of our database
+db = SQLAlchemy(app)
+
+# Create an instance of Migrate to handle the database migrations of our flask app
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes fil into the current package
 from app import routes
