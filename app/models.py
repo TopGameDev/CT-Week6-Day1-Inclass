c7568f44b9bd06c3f3ce2a30e8716ce507a411af
GameDev <gamemaster@GME.local>
2023-08-17 15:12:20 -0500

    Added token creation and Token revoke

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+import os
+import base64
 from app import db, login
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 from random import randint
 from flask_login import UserMixin
@@ -21,6 +23,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(50), nullable=False, unique=True)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     # Hash Password taken from User
     def __init__(self, **kwargs):
@@ -30,10 +34,28 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"<User {self.id}|{self.username}>"
     
+    # Validate Password
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
     
+    def get_token(self, expires_in=3600):
+        # Set the start of the timer
+        now = datetime.utcnow()
+        # Check for how long token has been active
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        # Create Token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        # Set Token Expiration
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        # Save Token to database and Execute
+        db.session.commit()
+        return self.token
     
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+        db.session.commit()
+
 @login.user_loader
 def load_user(user_id):
     return db.session.get(User, user_id)
